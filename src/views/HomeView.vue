0241ee7a537221515b2938e9235fccf0f016ea8c
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-09-02 12:29:01 +0200

    frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,29 @@
-<template>
+<template lang="">
   <div class="home">
-	<div class="bannertxt">
+	<div class="bannertxt mt-5 pt-5">
 		<h3>Your Getaway to</h3>
 		<h1>Comfort & Relaxation</h1>
+		<div class="book row">
+			<div class="col-lg-2 col-md-2 col-sm-12 pt-1">
+				<label>Check in:</label><br>
+				<input type="date">
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-12 pt-1">
+				<label>Check out:</label><br>
+				<input type="date">
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-12 pt-1">
+				<label>Adult(s):</label><br>
+				<input type="number" placeholder="2">
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-12 pt-1">
+				<label>Kid(s):</label><br>
+				<input type="number" placeholder="1">
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-12 align-content-center pt-1" >
+				<button class="btn">Book Now</button>
+			</div>
+		</div>
 	</div>
   </div>
 </template>
@@ -40,22 +61,18 @@ export default {
 @keyframes slideshow{
 	0%{
 		background-image: url("https://c0dingforfun.github.io/node-images/Images/Footer/background.jpg");
-    	/* height:80vh; */
 	}
 	
 	25%{
 		background-image: url("https://c0dingforfun.github.io/node-images/Images/Home/Carousel/One/one.jpg");
-    	/* height:80vh; */
 	}	
 	
 	50%{
 		background-image: url("https://c0dingforfun.github.io/capstone-images/Contact/contact.jpg");
-    	/* height:80vh; */
 	}
 	
 	100%{
 		background-image: url("https://c0dingforfun.github.io/node-images/Images/Home/Carousel/Five/five.jpg");
-    	/* height:80vh; */
 	}
 }
 
@@ -69,5 +86,31 @@ export default {
 
 .bannertxt h1{
 	font-size: 8vw;
+}
+.book{
+	border-radius:20px;
+	color:whitesmoke;
+	padding:1em;
+	display:flex;
+	position: relative;
+	justify-content: space-around;
+	/* width:80vw; */
+	background-color: #51b112;
+}
+.book input{
+	width:150px;
+	border-radius:20px;
+	text-align: center;
+}
+.book button{
+	border-radius:20px;
+	color:whitesmoke;
+	background-color: #bae39e;
+	color:black;
+	transition:0.8s;
+}
+.book button:hover{
+	background-color: #93C572;
+	color: whitesmoke;
 }
 </style>
