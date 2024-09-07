fdb46b10f5a566a55c442a60954df5572cd6e968
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-09-07 09:23:08 +0200

    backend

--- model/fullstackDB.js
+++ model/fullstackDB.js
@@ -38,19 +38,19 @@ const getRoomDB = async (room_id)=>{
     return data;
 }
 
-const insertRoomDB = async(user_name, user_surname, age, user_role, username, password, image)=>{
+const insertRoomDB = async(room_name,room_category,room_description,room_package,price,pets,image,subImage1,subImage2,subImage3)=>{
     let [data] = await pool.query(`
-        INSERT INTO users (user_name, user_surname, age, user_role, username, password, image)
-        VALUES(?,?,?,?,?,?,?)`,
-        [user_name, user_surname, age, user_role, username, password, image]
+        INSERT INTO users (room_name,room_category,room_description,room_package,price,pets,image,subImage1,subImage2,subImage3)
+        VALUES(?,?,?,?,?,?,?,?,?,?)`,
+        [room_name,room_category,room_description,room_package,price,pets,image,subImage1,subImage2,subImage3]
     )
 }
 
-const deleteRoomDB = async(user_id)=>{
-    await pool.query(`DELETE FROM users WHERE user_id = ?`,[user_id])
+const deleteRoomDB = async(room_id)=>{
+    await pool.query(`DELETE FROM rooms WHERE room_id = ?`,[room_id])
 }
 
-const updateRoomDB = async(user_name, user_surname, age, user_role, username, password, image,user_id )=>{
-    await pool.query(`UPDATE users SET user_name = ?, user_surname = ?, age = ?, user_role = ?, username = ?, password = ?, image = ? WHERE user_id = ?`,[user_name, user_surname, age, user_role, username, password, image,user_id])
+const updateRoomDB = async(room_name,room_category,room_description,room_package,price,pets,image,subImage1,subImage2,subImage3,room_id )=>{
+    await pool.query(`UPDATE users SET room_name = ?,room_category = ?,room_description = ?,room_package = ?,price = ?,pets = ?,image = ?,subImage1 = ?,subImage2 = ?,subImage3 = ? WHERE room_id = ?`,[room_name,room_category,room_description,room_package,price,pets,image,subImage1,subImage2,subImage3,room_id])
 }
 export {getUsersDB,getUserDB,insertUserDB,updateUserDB,deleteUserDB,getRoomsDB,getRoomDB,insertRoomDB,updateRoomDB,deleteRoomDB}
