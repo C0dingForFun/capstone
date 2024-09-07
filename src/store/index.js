1a9b755081b27579f2bf8500daa394d6b4e076c7
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-09-07 11:52:29 +0200

    frontend

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,35 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import axios from 'axios'
 
 export default createStore({
   state: {
+    users:null,
+    user:null,
+    rooms:null
   },
   getters: {
   },
   mutations: {
+    setUsers(state,payload){
+      state.users=payload
+    },
+    setUser(state,payload){
+      state.user=payload
+    },
+    setRooms(state,payload){
+      state.rooms=payload
+    }
   },
   actions: {
+    async getUsers({commit}){
+      let users = axios.get()
+    },
+    async getUsers({commit},id){
+      let user = axios.get()
+    },
+    async getUsers({commit}){
+      let users = axios.get()
+    },
   },
   modules: {
   }
