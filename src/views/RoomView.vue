9a104e436c4cb9b84c9edc63f7cd6c0a3705d5c1
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-09-09 12:18:45 +0200

    frontend

--- src/views/RoomView.vue
+++ src/views/RoomView.vue
@@ -1,16 +1,42 @@
 <template>
-    <div class="container-fluid mb-5">
-        <div class="row my-2" v-if="room()">
+    <div class="container-fluid mb-5 mt-5">
+        <div class="row mt-5" v-if="room()">
             <div class="row">
                 <h2 class="display-2">Product Details</h2>
             </div>
             <div class="row center">
                         <div class="card mt-5">
-                            <img :src="$store.state.room.image" class="card-img-top">
+                            <!-- <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item" v-for="(room,index) in room()" :key="index" :class="{'active':index==0}">
+                                        <img :src="$store.state.room.image" class="card-img-top">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img :src="$store.state.room.subImage1" class="card-img-top">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img :src="$store.state.room.subImage2" class="card-img-top">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img :src="$store.state.room.subImage3" class="card-img-top">
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button> -->
+                                <!-- </div> -->
                             <div class="card-body">
                                 <div class="prodName">
                                     <h5 class="card-title">{{$store.state.room.room_name}}</h5>
                                     <h4>{{$store.state.room.room_category}}</h4>
+                                </div>
+                                <div>
+                                    <p>{{$store.state.room.room_description}}</p>
                                 </div>
                                 <div>
                                     <p class="card-text">Price: R{{$store.state.room.price}}</p>
