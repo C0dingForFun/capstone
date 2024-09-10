c99fca9ce65894a612fed4fa7938fb2de61ab3b1
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-09-10 14:37:05 +0200

    frontend

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,21 +1,21 @@
 <template lang="">
     <footer>
-        <div class="newsletter">
-            <div class=" col-lg-4 col-md-4 col-sm-12">
+        <div class="newsletter row">
+            <div class=" col-lg-4 col-md-4 col-sm-12 mt-2">
                 <div>
-                    <a href=""
+                    <a href="https://www.google.com/maps/place/Lagoon+Beach+Hotel+%26+Spa/@-33.8925909,18.4832667,15z/data=!4m9!3m8!1s0x1dcc5c2ed4759665:0xe4c965c5195298f9!5m2!4m1!1i2!8m2!3d-33.8925909!4d18.4832667!16s%2Fg%2F11h565fllm?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D" target="blank"><p>1 Lagoon Gate Drive, Lagoon Beach, Cape Town, 7441</p></a>
                 </div>
                 <div>
-
+                   <i class="las la-tty"></i> <a href="tel:+27210098072"> +27 21 009 8072</a>
                 </div>
                 <div>
-
+                    <i class="las la-envelope"></i> <a href="mailto:support@coastalgetaway.co.za">support@coastalgetaway.co.za</a>
                 </div>
             </div>
-            <div class=" col-lg-4 col-md-4 col-sm-12">
-                
+            <div class=" col-lg-4 col-md-4 col-sm-12 mt-2">
+                <input type="text"/><button>Subscribe</button>
             </div>
-            <div class=" col-lg-4 col-md-4 col-sm-12">
+            <div class=" col-lg-4 col-md-4 col-sm-12 mt-2">
                 
             </div>
         </div>
@@ -34,5 +34,8 @@ export default {
         width:100vw;
         height:auto;
         background: #bae39e;
+    }
+    .newsletter input{
+        
     }
 </style>
