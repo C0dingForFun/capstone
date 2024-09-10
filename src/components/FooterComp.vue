b34a6ac0190abd17459981607624365d4b399eff
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-09-10 14:06:10 +0200

    frontend

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,38 @@
+<template lang="">
+    <footer>
+        <div class="newsletter">
+            <div class=" col-lg-4 col-md-4 col-sm-12">
+                <div>
+                    <a href=""
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+            <div class=" col-lg-4 col-md-4 col-sm-12">
+                
+            </div>
+            <div class=" col-lg-4 col-md-4 col-sm-12">
+                
+            </div>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .newsletter{
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        width:100vw;
+        height:auto;
+        background: #bae39e;
+    }
+</style>
