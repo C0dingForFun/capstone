be441171437b2003d8581e00858ead4f27333cca
C0dingForFun <gwibantulini8@gmail.com>
2024-09-11 10:59:49 +0200

    frontendd

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -2,7 +2,7 @@
     <footer>
         <div class="newsletter row">
             <div class=" col-lg-6 col-md-6 col-sm-12 mt-3">
-                <div>
+                <div class="link">
                     <i class="las la-map-marker"></i><a href="https://www.google.com/maps/place/Lagoon+Beach+Hotel+%26+Spa/@-33.8925909,18.4832667,15z/data=!4m9!3m8!1s0x1dcc5c2ed4759665:0xe4c965c5195298f9!5m2!4m1!1i2!8m2!3d-33.8925909!4d18.4832667!16s%2Fg%2F11h565fllm?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D" target="blank"><p>1 Lagoon Gate Drive, Lagoon Beach, Cape Town, 7441</p></a>
                 </div>
                 <div>
@@ -34,11 +34,11 @@ export default {
         width:100vw;
         height:auto;
     }
-    footer a{
+    footer link{
         color:whitesmoke;
         text-decoration: none;
     }
-    footer a:hover{
+    footer link:hover{
         color:#bae39e;
         text-decoration: underline;
     }
