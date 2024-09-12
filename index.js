f98d3e2ed3d25db3133497f480031f4ad1e4aa23
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-09-12 17:14:42 +0200

    backend

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ import { usersRouter,roomsRouter,bookedRouter} from './routes/fullstackRoute.js'
 
 const app = express();
 
-app.use(express.json());
-app.use(express.static('public'));
 app.use(cors({
     origin: ['http://localhost:8080', 'https://capstone-2xa4.onrender.com'],
     credentials:true,
-    headers:['Content-Type', 'Authorization','Allow-Control-Allow-Origin']
+    headers:['Content-Type', 'Authorization']
 }));
-app.use('/users/login')
+app.use('/users/login',(req,res)=>{
+    res.header('Allow-Control-Allow-Origin','http://localhost:8080')
+})
+app.use(express.json());
+app.use(express.static('public'));
 app.use('/users',usersRouter);
 app.use('/rooms',roomsRouter);
 app.use('/booked',bookedRouter);
