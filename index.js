e0249d46eb9bf68856d497776477a15c7c2687b9
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-09-13 09:36:12 +0200

    backend

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ app.use(cors({
 // app.use('/users/login',(req,res)=>{
 //     res.header('access-control-allow-origin','http://localhost:8080')
 // })
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+app.use(( req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Request-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "http://localhost:8080");
+    res.header("Access-Control-Request-Methods", "http://localhost:8080");
+    res.header("Access-Control-Allow-Headers", "http://localhost:8080");
     res.header("Access-Control-Expose-Headers", "Authorization");
 
-    next()
+    next();
 })
 app.use(express.json());
 app.use(express.static('public'));
