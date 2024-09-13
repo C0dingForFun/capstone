e8d2dd3395e3415bea471ae9dc513fafe77457f9
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-09-13 08:34:45 +0200

    backend

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,21 +1,21 @@
 import { compare } from "bcrypt";
-import { getUserDB2,getUserDB } from "../model/fullstackDB.js";
+import { getUserDB2 } from "../model/fullstackDB.js";
 import jwt from 'jsonwebtoken';
 import { config }from "dotenv";
 config();
 
 const checkUser = async (req,res,next)=>{
-    const {username, password} = req.body;
+    const {username, password,user_id} = req.body;
     let hashedPassword = (await getUserDB2(username)).password;
     let userRole = (await getUserDB2(username)).user_role;
-    let user_id = (await getUserDB(user_id)).user_id;
+    // let userID = (await getUserDB(userID)).user_id;
     let result = await compare(password, hashedPassword);
     if(result == true){
         let token = jwt.sign({username:username,userRole:userRole},process.env.SECRET_KEY, {expiresIn:'1h'});
         console.log(token);
         req.body.token = token;
         req.body.userRole = userRole
-        req.body.token = user_id;
+        // req.body.token = userID;
         next();
     }else{
         res.send('Password incorrect');
