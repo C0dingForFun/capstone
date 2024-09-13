486e48c2d3ccd7b0ef91237c988fc304b87efa13
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-09-13 12:19:03 +0200

    frontend

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ import { usersRouter,roomsRouter,bookedRouter} from './routes/fullstackRoute.js'
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:8080','*'],
+    origin: ['http://localhost:8080'],
     credentials:true,
 }));
 // app.use('/users/login',(req,res)=>{
 //     res.header('access-control-allow-origin','http://localhost:8080')
 // })
 app.use(( req, res, next) => {
-    res.header("Access-Control-Allow-Origin", ['http://localhost:8080','*']);
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", ['http://localhost:8080','*']);
-    res.header("Access-Control-Request-Methods", ['http://localhost:8080','*']);
-    res.header("Access-Control-Allow-Headers", ['http://localhost:8080','*']);
+    res.header("Access-Control-Allow-Methods", "http://localhost:8080");
+    res.header("Access-Control-Request-Methods", "http://localhost:8080");
+    res.header("Access-Control-Allow-Headers", "http://localhost:8080");
     res.header("Access-Control-Expose-Headers", "Authorization");
 
     next();
