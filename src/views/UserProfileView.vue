c1d27256b9f2505f75a9a00488324c7af1dd38ee
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-09-13 14:58:20 +0200

    frontend

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -1,18 +1,13 @@
 <template lang="">
-    <div>
+    <div class="profile d-flex justify-content-center align-items-center">
+
         <h2 style="text-align:center">User Profile Card</h2>
 
         <div class="card">
-            <img src="/w3images/team2.jpg" alt="John" style="width:100%">
+            <img :src="" alt="John" style="width:100%">
             <h1>John Doe</h1>
             <p class="title">CEO & Founder, Example</p>
             <p>Harvard University</p>
-            <div style="margin: 24px 0;">
-                <a href="#"><i class="fa fa-dribbble"></i></a> 
-                <a href="#"><i class="fa fa-twitter"></i></a>  
-                <a href="#"><i class="fa fa-linkedin"></i></a>  
-                <a href="#"><i class="fa fa-facebook"></i></a> 
-            </div>
             <p><button>Contact</button></p>
         </div>
     </div>
@@ -47,5 +42,8 @@ export default {
 }
 </script>
 <style scoped>
-    
+    .profile{
+        width:100vw;
+        height:100vh;
+    }
 </style>
